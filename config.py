7a128164f1446148b27e06af9e3a56cfc2657e64
Beyond DAO <14137098+RetroSoftLabs@users.noreply.github.com>
2024-07-04 20:00:41 +0300

    Update config.py
    update config comment

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ api_key = 'YOUR_API_KEY'
 api_secret = 'YOUR_API_SECRET'
 #Randomisation percent is a amount randomly added to your withdraw
 #Example: You have set randomisation_percent =3 and Amount 1 ETH. You will get between 1.000001 to 1.03 eth
-#Works only when Rand is working
+#Works only when Rand is set to enabled on the UI
 randomisation_percent = 3
